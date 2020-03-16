d4fd8ea5a9d68fe240edb73697e338226ec79325
Isabella <isabella.soares.28@gmail.com>
2020-03-15 22:17:32 -0300

    add new pages

---
+++ call-of-papers/src/pages/Event.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Event = () => <h2>Event Page</h2>;
+
+export default Event;
---
+++ call-of-papers/src/pages/Lecture.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Lecture = () => <h2>Lecture Page</h2>;
+
+export default Lecture;
