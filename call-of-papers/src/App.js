cdbe04be38ad5656a33b02a999ceab65af6da51d
Isabella <isabella.soares.28@gmail.com>
2020-03-16 21:25:14 -0300

    add Ant layout

--- call-of-papers/src/App.js
+++ call-of-papers/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Routes from './Routes';
+import { Layout } from 'antd';
 import './App.css';
+
+const { Header, Footer, Content } = Layout;
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Menu</h1>
-      <Routes /> 
+      <Layout>
+        <Header style={{color: '#FFF'}}>Menu</Header>
+        <Content>
+          <Routes/>
+        </Content>
+        <Footer>Footer</Footer>
+      </Layout>
     </div>
   )
 }
