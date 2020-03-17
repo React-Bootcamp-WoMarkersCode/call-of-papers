eb5b6cad54348680d2b8fb1c270f82b2e5fdef72
Isabella <isabella.soares.28@gmail.com>
2020-03-16 21:17:47 -0300

    add Ant Design

--- call-of-papers/src/index.js
+++ call-of-papers/src/index.js
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'antd/dist/antd.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
