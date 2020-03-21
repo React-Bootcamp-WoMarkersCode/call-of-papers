87babf5bfa7bab29f6c0f63aa2c08a09a354c58e
Isabella <isabella.soares.28@gmail.com>
2020-03-21 16:19:43 -0300

    add grids system

--- call-of-papers/src/pages/MyProfile.js
+++ call-of-papers/src/pages/MyProfile.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { useParams } from 'react-router';
+import { Row, Col, Avatar, Typography } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+const { Title } = Typography;
 
 const MyProfile = () => {
     let { profileId } = useParams();
-    
+
     return (
-    <h2>My Profile ID: {profileId}</h2>
+      <>
+      <Row>
+        <Col span={16} offset={4}>
+          <h2>My Profile ID: {profileId}</h2>
+        </Col>
+        <Col span={16} offset={4} style={{backgroundColor: '#FFF'}}>
+          <Row>
+            <Col span={6} style={{backgroundColor: '#000', textAlign: 'center'}}>
+              <Avatar shape="square" size={100} icon={<UserOutlined />} />
+            </Col>
+            <Col span={18}>
+              <Title level={4}>Username</Title>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      </>
     );
 };
 
