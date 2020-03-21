ff40938f669ed139cfb1aa5c45ed93b03acd1c99
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2020-03-21 11:31:31 -0300

    event-list: Criação da tela

--- call-of-papers/src/pages/Events/EventsList.js
+++ call-of-papers/src/pages/Events/EventsList.js
@@ -1,5 +1,69 @@
 import React from 'react';
+import { Row, Col, Table, Tag } from 'antd';
 
-const EventsList = () => <h2>Events List</h2>;
+const EventsList = () => {
+
+    const columns = [
+        {
+            title: 'Data/Horário',
+            dataIndex: 'data',
+            key: 'data'
+        },
+        {
+            title: 'Eventos',
+            dataIndex: 'evento',
+            key: 'evento'
+        },
+        {
+            title: 'Local',
+            dataIndex: 'local',
+            key: 'local'
+        },
+        {
+            title: '',
+            dataIndex: 'acao',
+            key: 'acao',
+            render: (text) => (
+                <span>
+                    <a>Detalhes</a>
+                </span>
+            )
+        },
+    ];
+
+    const data = [
+        {
+            key: '1',
+            data: '21 Mar, 07:30',
+            evento: 'Women Dev Summit',
+            local: 'Secretaria',
+        },
+        {
+            key: '2',
+            data: '21 Mar, 07:30',
+            evento: 'Women Dev Summit',
+            local: 'Secretaria',
+        },
+    ]
+
+    return (
+        <>
+            <Row>
+                <Col span={4}></Col>
+                <Col span={16}>
+                    <h1>Meus eventos</h1>
+                </Col>
+                <Col span={4}></Col>
+            </Row>
+            <Row>
+                <Col span={4}></Col>
+                <Col span={16}>
+                    <Table columns={columns} dataSource={data} />
+                </Col>
+                <Col span={4}></Col>
+            </Row>
+        </>
+    );
+}
 
 export default EventsList;
