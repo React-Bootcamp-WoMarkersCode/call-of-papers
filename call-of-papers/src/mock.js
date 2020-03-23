79c77933ee1c24e6ea12a5fd7078657b0e38da77
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2020-03-23 17:37:07 -0300

    Tela de "Lista de Eventos" (#24)
    * event-list: Criação da tela
    
    * event-list: Correção para pull request

---
+++ call-of-papers/src/mock.js
@@ -0,0 +1,14 @@
+export const events = [
+    {
+        key: '1',
+        data: '21 Mar, 07:30',
+        evento: 'Women Dev Summit',
+        local: 'Secretaria',
+    },
+    {
+        key: '2',
+        data: '21 Mar, 07:30',
+        evento: 'Women Dev Summit',
+        local: 'Secretaria',
+    },
+];
--- call-of-papers/src/pages/Events/EventsList.js
+++ call-of-papers/src/pages/Events/EventsList.js
@@ -1,5 +1,48 @@
 import React from 'react';
+import { Row, Col, Table } from 'antd';
+import { Link } from 'react-router-dom';
 
-const EventsList = () => <h2>Events List</h2>;
+import { events } from "../../mock";
+
+const EventsList = () => {
+
+    const columnsTable = [
+        {
+            title: 'Data/Horário',
+            dataIndex: 'data',
+            key: 'data'
+        },
+        {
+            title: 'Eventos',
+            dataIndex: 'evento',
+            key: 'evento'
+        },
+        {
+            title: 'Local',
+            dataIndex: 'local',
+            key: 'local'
+        },
+        {
+            dataIndex: 'acao',
+            key: 'acao',
+            render: (text) => (
+                <span>
+                    <Link to="/events/1">Detalhes</Link>
+                </span>
+            )
+        },
+    ];
+
+    return (
+        <>
+            <Row>
+                <Col span={16} offset={4}>
+                    <h1>Meus eventos</h1>
+                    <Table columns={columnsTable} dataSource={events} />
+                </Col>
+            </Row>
+        </>
+    );
+}
 
 export default EventsList;
