108c01a7a11811bc422b3c966e0ff076f9fa74a6
Kathleen Pallotta <kathpallotta@gmail.com>
2020-03-26 22:37:25 -0300

    Alterações no CSS e página de partners

--- call-of-papers/src/App.js
+++ call-of-papers/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Routes from './Routes';
 import { Layout } from 'antd';
 import Menu from './components/Menu';
-import Partners from './pages/Events/Partners';
 
 const { Header, Footer, Content } = Layout;
 
