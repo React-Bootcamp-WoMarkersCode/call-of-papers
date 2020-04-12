799fbbb3f48c87e4b3dc6e9e48744bd189a3ac62
Isabella <isabella.soares.28@gmail.com>
2020-04-12 18:56:01 -0300

    Revert issue #40

--- call-of-papers/src/App.js
+++ call-of-papers/src/App.js
@@ -4,13 +4,14 @@ import { Layout, Button } from 'antd'
 import Menu from './components/Menu'
 import Rodape from './components/Footer'
 import Partners from './pages/Events/Partners'
+import 'antd/dist/antd.css'
 
 const { Header, Footer, Content } = Layout
 
 const App = () => {
 	return (
 		<Layout style={{minHeight: "calc(100vh - 70px)"}}>
-			<Header>
+			<Header style={{boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.20)'}}>
 				<Menu />
 			</Header>
 			<Content style={{padding: '30px 0'}}>
