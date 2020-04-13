5b5e4076da9f358d9554ef1ee63308a7c1c5de56
Isabella <isabella.soares.28@gmail.com>
2020-04-13 19:41:04 -0300

    Refactor: padronize lectures list

--- call-of-papers/src/pages/Lectures/LecturesList.js
+++ call-of-papers/src/pages/Lectures/LecturesList.js
@@ -1,37 +1,74 @@
 import React from 'react'
-import { List } from 'antd'
+import { Row, Table, Divider, Tag } from 'antd'
 import { Link } from 'react-router-dom'
 import './lectures-list.scss'
 import data from './lectures-list-test.json'
 
-const LecturesList = () => (
-  <div className='listed'>
-    <List
-      pagination
-      itemLayout='horizontal'
-      dataSource={data}
-      renderItem={item => (
-        <List.Item>
-          <table>
-            <tr></tr>
-            <tr>
-              <Link to={`/lectures/${item.id}`}>
-                <td className='listed__lecture'>
-                  <h3>{item.title}</h3>
-                  <p>
-                    {item.description}
-                  </p>
-                </td>
-                <td className={'listed__lecture-status--' + item.status}>
-                  {item.status.toUpperCase()}
-                </td>
-              </Link>
-            </tr>
-          </table>
-        </List.Item>
-      )}
-    />
-  </div>
-)
+const columnsTable = [
+  {
+    title: 'Título',
+    dataIndex: 'title',
+    key: 'title',
+    width: '20%'
+  },
+  {
+    title: 'Descrição',
+    dataIndex: 'description',
+    key: 'description',
+    width: '55%'
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    key: 'status',
+    className: 'status-cell',
+    render: status => {
+      let color = '';
+
+      if (status === 'aprovado') {
+        color = 'green';
+      } else if (status === 'em análise') {
+        color = 'geekblue';
+      } else {
+        color = 'volcano';
+      }
+
+      return (
+        <Tag color={color} key={status}>
+          {status.toUpperCase()}
+        </Tag>
+      );
+    }
+  },
+  {
+    dataIndex: 'id',
+    key: 'id',
+    render: (key) => (
+      <span>
+        <Link to={`/events/${key}`}>Mais detalhes</Link>
+      </span>
+    )
+  },
+]
+
+const LecturesList = () => {
+  return (
+    <>
+      <Row gutter={[16, 24]}>
+        <Divider orientation="left">
+          Minhas palestras
+        </Divider>
+      </Row>
+      <Row justify="center" className='row-table'>
+        <Table
+          columns={columnsTable}
+          dataSource={data}
+          rowKey='id'
+          size='middle'
+          pagination={{ pageSize: 10 }} />
+      </Row>
+    </>
+  )
+}
 
 export default LecturesList
