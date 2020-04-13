c14605dffb301b7f3f8e532fda23174b7c51afce
Isabella <isabella.soares.28@gmail.com>
2020-04-12 21:38:05 -0300

    Refactor: padronize home

--- call-of-papers/src/pages/Home/CardEvent.js
+++ call-of-papers/src/pages/Home/CardEvent.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { Col, Card } from 'antd'
+import { Card } from 'antd'
+import { Link } from 'react-router-dom'
+
+const { Meta } = Card;
 
 function CardEvent({ event }) {
-    return (
-        <Col span={8}>
-            <Card
-                hoverable
-                title={event.evento}
-                bordered={true}
-                bodyStyle={{ maxHeight: '150px' }}
-            >
-                <p >{event.data}</p>
-            </Card>
-        </Col>
-    )
+	return (
+    <Card
+      cover={<img alt={event.evento} src={event.img} />}
+      actions={[<Link to="/about">Saiba mais!</Link>]}
+    >
+      <Meta title={event.evento} description={event.data} />
+    </Card>
+	);
 }
 
-export default CardEvent
+export default CardEvent;
+
--- call-of-papers/src/pages/Home/Home.js
+++ call-of-papers/src/pages/Home/Home.js
@@ -3,24 +3,25 @@ import { Row } from 'antd'
 import { events } from '../Events/events-list-test'
 import CardEvent from './CardEvent'
 import FBLogin from '../Login/FBLogin'
+import './style.scss'
 
 function Home() {
-
-    return (
-        <>
-            <div style={{float:'right'}}><FBLogin /></div>
-            <br></br>
-            <div><h1 style={{padding: '10px', marginLeft:'200px'}}>Eventos </h1></div>
-            <Row justify="center" gutter={[16, 24]} style={{ margin: '0 70px' }}>
-                {events.map((event) => {
-                    return (
-                        <CardEvent event={event} />
-                    )
-                })}
-            </Row>
-        </>
-    )
-
+  return (
+    <>
+      <div style={{float:'right'}}><FBLogin /></div>
+      <br></br>
+      <Row gutter={[16, 24]}>
+        <h1>Eventos</h1>
+      </Row>
+      <Row justify="center" gutter={[16, 24]}>
+        {events.map((event) => {
+          return (
+            <CardEvent event={event} />
+          )
+        })}
+      </Row>
+    </>
+  )
 }
 
 export default Home
