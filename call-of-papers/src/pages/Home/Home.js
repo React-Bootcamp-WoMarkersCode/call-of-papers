be85e920def60abaed1d02418a99cfbeab49c394
Isabella <isabella.soares.28@gmail.com>
2020-04-12 23:20:27 -0300

    Refactor: correct Facebook login pop-up import

--- call-of-papers/src/pages/Home/Home.js
+++ call-of-papers/src/pages/Home/Home.js
@@ -2,14 +2,11 @@ import React from 'react'
 import { Row } from 'antd'
 import { events } from '../Events/events-list-test'
 import CardEvent from './CardEvent'
-import FBLogin from '../Login/FBLogin'
 import './style.scss'
 
 const Home = () => {
   return (
     <>
-      <div style={{float:'right'}}><FBLogin /></div>
-      <br></br>
       <Row gutter={[16, 24]}>
         <h1>Eventos</h1>
       </Row>
