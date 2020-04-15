7772a5df20ad37775c506568616002a93502456f
Olívia Resende <olivinharesende@hotmail.com>
2020-04-15 11:08:46 -0300

    Melhoria tela about

--- call-of-papers/src/pages/About/index.js
+++ call-of-papers/src/pages/About/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Row, Divider, List, Col } from 'antd'
-
+import { LinkedinOutlined, GithubOutlined} from '@ant-design/icons'
 const About = () => {
     return(
       <>
@@ -10,22 +10,21 @@ const About = () => {
           </Divider>
         </Row>
         <Row gutter={[16, 24]} className='home-cards-container'>
-          <Col span={16} offset={2}>
-            <p>Projeto final para conclusão do React Bootcamp da WoMakersCode.</p>
-            <p>
-              <a href="https://github.com/React-Bootcamp-WoMarkersCode/call-of-papers/tree/master/call-of-papers">
-                Link do projeto no Github
-              </a>
+          <Col span={12} offset={4} style={{ textAlign: 'justify' }}>
+            <p><i>Call for Papers</i> é uma plataforma para conectar eventos a palestrantes.
+              Possibilitando que organizadores criem eventos em busca de palestrantes que tenham interesse naquele evento. Para os palestrante esta disponivel o cadastro de informações pertinentes ao seu tema. Existe também a opção para patrocinadores e voluntários que tenham interesse em ajudar na realização do evento.</p>
+            <p>Projeto final desenvolvido no React Bootcamp da WoMakersCode como forma de aplicar os conhecimentos adquiridos ao longo do curso.
+              Para acessar o código desenvolvido no <a href="https://github.com/React-Bootcamp-WoMarkersCode/call-of-papers/tree/master/call-of-papers">GitHub <GithubOutlined />.</a>
             </p>
-          </Col>
-          <Col span={16} offset={2}>
             <List>
-              <h3>Desenvolvidor por:</h3>
-              <List.Item>Alessandra Nastassja: <a href="https://github.com/Alessandra-Nastassja">Github</a> e <a href="https://www.linkedin.com/in/alessandra-nastassja/">Linkedin</a></List.Item>
-              <List.Item>Isabella Soares: <a href="https://github.com/IsabellaSoares">Github</a> e <a href="https://www.linkedin.com/in/isabella-soares-lima/">Linkedin</a></List.Item>
-              <List.Item>Kathleen Pallotta: <a href="https://github.com/kathleenpallotta">Github</a> e <a href="https://www.linkedin.com/in/kathleen-pallotta-ab8346199/">Linkedin</a></List.Item>
-              <List.Item>Mariana Tancredi: <a href="https://github.com/matancredi">Github</a> e <a href="https://www.linkedin.com/in/mariana-t-52aa8b8b/">Linkedin</a></List.Item>
-              <List.Item>Olívia Resende: <a href="https://github.com/oliviaresende">Github</a> e <a href="https://www.linkedin.com/in/oliviasilvaresende/">Linkedin</a></List.Item>
+              <Col span={12}>
+              <List.Item>Desenvolvedoras:</List.Item>
+              <List.Item>Alessandra Nastassja: <a href="https://github.com/Alessandra-Nastassja">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/alessandra-nastassja/">Linkedin <LinkedinOutlined /></a></List.Item>
+              <List.Item>Isabella Soares: <a href="https://github.com/IsabellaSoares">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/isabella-soares-lima/">Linkedin <LinkedinOutlined /></a></List.Item>
+              <List.Item>Kathleen Pallotta: <a href="https://github.com/kathleenpallotta">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/kathleen-pallotta-ab8346199/">Linkedin <LinkedinOutlined /></a></List.Item>
+              <List.Item>Mariana Tancredi: <a href="https://github.com/matancredi">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/mariana-t-52aa8b8b/">Linkedin <LinkedinOutlined /></a></List.Item>
+              <List.Item>Olívia Resende: <a href="https://github.com/oliviaresende">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/oliviasilvaresende/">Linkedin <LinkedinOutlined /></a></List.Item>
+              </Col>
             </List>
           </Col>
         </Row>
