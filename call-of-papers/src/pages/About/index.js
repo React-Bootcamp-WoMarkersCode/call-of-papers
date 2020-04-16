a1beff95f206c3ee87b244baf5ec709fee3d9c81
Isabella Soares <isabella.lima@goomer.com.br>
2020-04-16 15:45:12 -0300

    Style: correct About page layout

--- call-of-papers/src/pages/About/index.js
+++ call-of-papers/src/pages/About/index.js
@@ -10,7 +10,7 @@ const About = () => {
           </Divider>
         </Row>
         <Row gutter={[16, 24]} className='home-cards-container'>
-          <Col span={12} offset={4} style={{ textAlign: 'justify' }}>
+          <Col span={16} offset={4} style={{ textAlign: 'justify' }}>
             <p><i>Call for Papers</i> é uma plataforma para conectar eventos a palestrantes.
               Possibilitando que organizadores criem eventos em busca de palestrantes que tenham interesse naquele evento. Para os palestrante esta disponivel o cadastro de informações pertinentes ao seu tema. Existe também a opção para patrocinadores e voluntários que tenham interesse em ajudar na realização do evento.</p>
             <p>Projeto final desenvolvido no React Bootcamp da WoMakersCode como forma de aplicar os conhecimentos adquiridos ao longo do curso.
@@ -18,7 +18,7 @@ const About = () => {
             </p>
             <List>
               <Col span={12}>
-              <List.Item>Desenvolvedoras:</List.Item>
+              <h3>Desenvolvedoras</h3>
               <List.Item>Alessandra Nastassja: <a href="https://github.com/Alessandra-Nastassja">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/alessandra-nastassja/">Linkedin <LinkedinOutlined /></a></List.Item>
               <List.Item>Isabella Soares: <a href="https://github.com/IsabellaSoares">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/isabella-soares-lima/">Linkedin <LinkedinOutlined /></a></List.Item>
               <List.Item>Kathleen Pallotta: <a href="https://github.com/kathleenpallotta">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/kathleen-pallotta-ab8346199/">Linkedin <LinkedinOutlined /></a></List.Item>
