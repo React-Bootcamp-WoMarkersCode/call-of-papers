33dc93f6ac7fc0995bd6a2609a4fe1272ebb685e
Mariana Tancredi Alvares Fernandes <matancredi@hotmail.com>
2020-04-17 00:15:07 -0300

    Updates

---
+++ precache-manifest.1d60f360292bcf17deb86f12a4f45d63.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e81f9761ba1aec398451335af4a9cd",
+    "url": "/call-of-papers/index.html"
+  },
+  {
+    "revision": "2044d39f7c03b6f8f508",
+    "url": "/call-of-papers/static/css/10.a2049f82.chunk.css"
+  },
+  {
+    "revision": "3336b7854f3aa8253ce0",
+    "url": "/call-of-papers/static/css/12.1230892a.chunk.css"
+  },
+  {
+    "revision": "a9b8926ce0f156e24313",
+    "url": "/call-of-papers/static/css/14.9ae03674.chunk.css"
+  },
+  {
+    "revision": "df272ffdbd099ef2ea83",
+    "url": "/call-of-papers/static/css/15.f2d1079b.chunk.css"
+  },
+  {
+    "revision": "67e987e4242b9324b943",
+    "url": "/call-of-papers/static/css/16.99b963da.chunk.css"
+  },
+  {
+    "revision": "bd653acd4933a563500d",
+    "url": "/call-of-papers/static/css/20.d3f31830.chunk.css"
+  },
+  {
+    "revision": "713cdf9d95d5aed3d575",
+    "url": "/call-of-papers/static/css/22.9ae03674.chunk.css"
+  },
+  {
+    "revision": "c22e7932900867368c87",
+    "url": "/call-of-papers/static/css/8.4af70dfe.chunk.css"
+  },
+  {
+    "revision": "60d92836111ae9212bab",
+    "url": "/call-of-papers/static/css/9.cdb298ac.chunk.css"
+  },
+  {
+    "revision": "6d2c751c07a56c34d97d",
+    "url": "/call-of-papers/static/css/main.622303d6.chunk.css"
+  },
+  {
+    "revision": "193bed8b468f08a95efb",
+    "url": "/call-of-papers/static/js/0.0dafaf91.chunk.js"
+  },
+  {
+    "revision": "91288c41def562490160",
+    "url": "/call-of-papers/static/js/1.c0817b6c.chunk.js"
+  },
+  {
+    "revision": "2044d39f7c03b6f8f508",
+    "url": "/call-of-papers/static/js/10.8968515b.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/call-of-papers/static/js/10.8968515b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc08ab10206fe31b7bb6",
+    "url": "/call-of-papers/static/js/11.1f48fa2d.chunk.js"
+  },
+  {
+    "revision": "3336b7854f3aa8253ce0",
+    "url": "/call-of-papers/static/js/12.6ddfd50a.chunk.js"
+  },
+  {
+    "revision": "97be0c2f7933bc394c6b",
+    "url": "/call-of-papers/static/js/13.f19af55c.chunk.js"
+  },
+  {
+    "revision": "a9b8926ce0f156e24313",
+    "url": "/call-of-papers/static/js/14.57bd90f6.chunk.js"
+  },
+  {
+    "revision": "df272ffdbd099ef2ea83",
+    "url": "/call-of-papers/static/js/15.34b47d5f.chunk.js"
+  },
+  {
+    "revision": "67e987e4242b9324b943",
+    "url": "/call-of-papers/static/js/16.74967d86.chunk.js"
+  },
+  {
+    "revision": "6a7dcd7d1c04495633fb",
+    "url": "/call-of-papers/static/js/17.327bb468.chunk.js"
+  },
+  {
+    "revision": "8c8566c49d108676e24d",
+    "url": "/call-of-papers/static/js/18.e4ac8a3e.chunk.js"
+  },
+  {
+    "revision": "95f911f61ecdc19da407",
+    "url": "/call-of-papers/static/js/19.2318fcd5.chunk.js"
+  },
+  {
+    "revision": "142c0f80869f9fe81039",
+    "url": "/call-of-papers/static/js/2.c5dd3991.chunk.js"
+  },
+  {
+    "revision": "bd653acd4933a563500d",
+    "url": "/call-of-papers/static/js/20.84514815.chunk.js"
+  },
+  {
+    "revision": "300009f0b5fd1445888b",
+    "url": "/call-of-papers/static/js/21.e60d0952.chunk.js"
+  },
+  {
+    "revision": "713cdf9d95d5aed3d575",
+    "url": "/call-of-papers/static/js/22.625f6b23.chunk.js"
+  },
+  {
+    "revision": "246cdb03ab6aed648fa6",
+    "url": "/call-of-papers/static/js/23.203c98a0.chunk.js"
+  },
+  {
+    "revision": "9187e64cce25af28effb",
+    "url": "/call-of-papers/static/js/3.861aefcd.chunk.js"
+  },
+  {
+    "revision": "6f10ee262020d1097de6",
+    "url": "/call-of-papers/static/js/4.0223cccd.chunk.js"
+  },
+  {
+    "revision": "6d2be47f49d2d802b5e7",
+    "url": "/call-of-papers/static/js/5.2991e69b.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/call-of-papers/static/js/5.2991e69b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22e7932900867368c87",
+    "url": "/call-of-papers/static/js/8.18b9b93e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/call-of-papers/static/js/8.18b9b93e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d92836111ae9212bab",
+    "url": "/call-of-papers/static/js/9.38a5c429.chunk.js"
+  },
+  {
+    "revision": "6d2c751c07a56c34d97d",
+    "url": "/call-of-papers/static/js/main.a82840fd.chunk.js"
+  },
+  {
+    "revision": "08791e00fc0f4f06d654",
+    "url": "/call-of-papers/static/js/runtime-main.bbbc0775.js"
+  },
+  {
+    "revision": "942afd57e36c342898fa965a14def8c0",
+    "url": "/call-of-papers/static/media/logo.942afd57.jpeg"
+  }
+]);
