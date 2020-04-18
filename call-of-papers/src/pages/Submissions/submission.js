1b67bd96ad30cc0573e79cc9fe5407acd9afd36c
Isabella <isabella.soares.28@gmail.com>
2020-04-17 21:13:49 -0300

    WIP - refactor submission page

--- call-of-papers/src/pages/Submissions/submission.js
+++ call-of-papers/src/pages/Submissions/submission.js
@@ -49,71 +49,79 @@ const Submission = () => {
           Avaliação de Atividade
         </Divider>
       </Row>
-			<Row justify='center'>
-				<Col span={4} style={{ textAlign: 'center', paddingRight: '10px' }}>
+			<Row justify='center' style={{ marginBottom: '2em' }}>
+				<Col span={4}>
 					<br />
 					<Avatar shape="square" size={50} src={userPicture} />
 					<br />
-					<p className="palestrante-infos">Nome: {api.name}</p>
-					<p className="palestrante-infos">Telefone: {api.cellphone}</p>
-					<p className="palestrante-infos">Email: {api.email}</p>
-					<p className="palestrante-infos">Minibiografia: {api.miniBio}</p>
-					<div>
-						{api.linkedin ?
-							<a href={`${api.linkedin}`}><LinkedinOutlined className="social-network" /></a>
-							: <LinkedinOutlined className="social-network" />}
-						{api.facebook ?
-							<a href={`${api.facebook}`}><FacebookOutlined className="social-network" /></a>
-							: <FacebookOutlined className="social-network" />}
-						{api.twitter ?
-							<a href={`${api.twitter}`}><TwitterOutlined className="social-network" /></a>
-							: <TwitterOutlined className="social-network" />}
-						{api.instagram ?
-							<a href={`${api.instagram}`}><InstagramOutlined className="social-network" /></a>
-							: <InstagramOutlined className="social-network" />}
-						{api.youtube ?
-							<a href={`${api.youtube}`}><YoutubeOutlined className="social-network" /></a>
-							: <YoutubeOutlined className="social-network" />}
-						{api.portfolio ?
-							<a href={`${api.portfolio}`}><FolderOutlined className="social-network" /></a>
-							: <FolderOutlined className="social-network" />}
-					</div>
-
-
+          <Descriptions layout="vertical">
+            <Item label="Nome" span={3}>
+              {api.name? `${api.name}` : 'Sem dados'}
+            </Item>
+            <Item label="Telefone" span={3}>
+              {api.cellphone? `${api.cellphone}` : 'Sem dados'}
+            </Item>
+            <Item label="E-mail" span={3} style={{wordBreak: 'break-word'}}>
+              {api.email? `${api.email}` : 'Sem dados'}
+            </Item>
+            <Item label="Mini biografia" span={3}>
+              {api.miniBio? `${api.miniBio}` : 'Sem dados'}
+            </Item>
+            <Item>
+              {api.linkedin ?
+                <a href={`${api.linkedin}`}><LinkedinOutlined className="social-network" /></a>
+                : <LinkedinOutlined className="social-network" />}
+              {api.facebook ?
+                <a href={`${api.facebook}`}><FacebookOutlined className="social-network" /></a>
+                : <FacebookOutlined className="social-network" />}
+              {api.twitter ?
+                <a href={`${api.twitter}`}><TwitterOutlined className="social-network" /></a>
+                : <TwitterOutlined className="social-network" />}
+              {api.instagram ?
+                <a href={`${api.instagram}`}><InstagramOutlined className="social-network" /></a>
+                : <InstagramOutlined className="social-network" />}
+              {api.youtube ?
+                <a href={`${api.youtube}`}><YoutubeOutlined className="social-network" /></a>
+                : <YoutubeOutlined className="social-network" />}
+              {api.portfolio ?
+                <a href={`${api.portfolio}`}><FolderOutlined className="social-network" /></a>
+                : <FolderOutlined className="social-network" />}
+            </Item>
+          </Descriptions>
 				</Col>
 				<Col span={12}>
-        <Descriptions layout="vertical" title={api.activityTitle}>
-          <Item label="Descrição" span={3}>
-            {api.activityDescription? `${api.activityDescription}` : 'Sem dados'}
-          </Item>
-          <Item label="Tipo" span={1}>
-            {api.activityType ? `${(api.activityType)}` : 'Sem dados'}
-          </Item>
-          <Item label="Já palestrou?" span={2}>
-            {api.haveLecturedBefore ? `${(api.haveLecturedBefore)}` : 'Sem dados'}
-          </Item>
-          <Item label="Categorias" span={3}>
-            {api.activityCategory ? `${(api.activityCategory)} e ${(api.activityCategory)}` : 'Sem dados'}
-          </Item>
-          <Item label="Avaliação" span={3}>
-            { api.length !== 0 ?
-              <Select
-                style={{ width: 150, textTransform: 'uppercase' }}
-                value={status}
-                onChange={handleChange}
-              >
-                <Option value="em análise">Em Análise</Option>
-                <Option value="aprovado">Aprovado</Option>
-                <Option value="reprovado">Reprovado</Option>
-              </Select>
-                :
-              <Spin size='large' />
-            }
-          </Item>
-          <Item span={3}>
-            <Button type='primary' onClick={submitEvaluation}>Enviar avaliação</Button>
-          </Item>
-        </Descriptions>
+          <Descriptions layout="vertical" title={api.activityTitle}>
+            <Item label="Descrição" span={3}>
+              {api.activityDescription? `${api.activityDescription}` : 'Sem dados'}
+            </Item>
+            <Item label="Tipo" span={1}>
+              {api.activityType ? `${(api.activityType)}` : 'Sem dados'}
+            </Item>
+            <Item label="Já palestrou?" span={2}>
+              {api.haveLecturedBefore ? `${(api.haveLecturedBefore)}` : 'Sem dados'}
+            </Item>
+            <Item label="Categorias" span={3}>
+              {api.activityCategory ? `${(api.activityCategory)} e ${(api.activityCategory)}` : 'Sem dados'}
+            </Item>
+            <Item label="Avaliação" span={3}>
+              { api.length !== 0 ?
+                <Select
+                  style={{ width: 150, textTransform: 'uppercase' }}
+                  value={status}
+                  onChange={handleChange}
+                >
+                  <Option value="em análise">Em Análise</Option>
+                  <Option value="aprovado">Aprovado</Option>
+                  <Option value="reprovado">Reprovado</Option>
+                </Select>
+                  :
+                <Spin size='large' />
+              }
+            </Item>
+            <Item span={3}>
+              <Button type='primary' onClick={submitEvaluation}>Enviar avaliação</Button>
+            </Item>
+          </Descriptions>
 				</Col>
 			</Row>
 		</>
