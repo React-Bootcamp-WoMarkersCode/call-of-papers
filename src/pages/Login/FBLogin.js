0ede03463d9470a9ba43e5cf29d0ba37e0b64abe
GabrieleSuzart <gabrielesuzart@gmail.com>
2020-04-20 09:45:02 -0300

    Fix security

--- src/pages/Login/FBLogin.js
+++ src/pages/Login/FBLogin.js
@@ -5,57 +5,57 @@ import { getCurrentDate } from './../../utils/currentDate'
 import { getEnvironment } from './../../utils/environment'
 
 const FBLogin = ({ isDisabled }) => {
-	let history = useHistory()
+  let history = useHistory()
   const environment = getEnvironment()
 
-	const responseFacebook = (response) => {
-		// Armazena os dados do usuário no navegador. Quando houver logout eles serão apagados
-		localStorage.setItem('userId', response.userID)
-		localStorage.setItem('userPicture', response.picture.data.url)
-		localStorage.setItem('userName', response.name)
-		localStorage.setItem('userEmail', response.email)
+  const responseFacebook = (response) => {
+    // Armazena os dados do usuário no navegador. Quando houver logout eles serão apagados
+    localStorage.setItem('userId', response.userID)
+    localStorage.setItem('userPicture', response.picture.data.url)
+    localStorage.setItem('userName', response.name)
+    localStorage.setItem('userEmail', response.email)
 
-		// Redireciona para a página inicial
-		history.push('/')
+    // Redireciona para a página inicial
+    history.push('/')
 
-		// Verifica se é um novo usuário ou não. Se for, adiciona no json de usuários
-		fetch(`${environment}/profiles`)
-			.then((res) => res.json())
-			.then((data) => {
-				if (!data.find((profile) => profile.email === response.email)) {
-					let newProfile = {
-						id: response.userID,
-						email: response.email? response.email : '',
-						localization: '',
-						registerDate: `${getCurrentDate()}`,
-						apresentation: '',
-						githubLink: '',
-						linkedinLink: '',
-						twitterLink: '',
-						facebookLink: '',
-						mediumLink: '',
-						interests: []
-					}
-					fetch(`${environment}/profiles`, {
-						method: 'post',
-						headers: {
-							Accept: 'application/json',
-							'Content-Type': 'application/json'
-						},
-						body: JSON.stringify(newProfile)
-					})
-						.then(function(response) {
-							console.log(response)
-						})
-						.catch((err) => console.error(err, 'Não foi possível criar usuário'))
-				}
-			})
-			.catch((err) => console.error(err, 'Nenhum usuário encontrado'))
-	}
+    // Verifica se é um novo usuário ou não. Se for, adiciona no json de usuários
+    fetch(`${environment}/profiles`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.find((profile) => profile.email === response.email)) {
+          let newProfile = {
+            id: response.userID,
+            email: response.email? response.email : '',
+            localization: '',
+            registerDate: `${getCurrentDate()}`,
+            apresentation: '',
+            githubLink: '',
+            linkedinLink: '',
+            twitterLink: '',
+            facebookLink: '',
+            mediumLink: '',
+            interests: []
+          }
+          fetch(`${environment}/profiles`, {
+            method: 'post',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newProfile)
+          })
+            .then(function(response) {
+              console.log(response)
+            })
+            .catch((err) => console.error(err, 'Não foi possível criar usuário'))
+        }
+      })
+      .catch((err) => console.error(err, 'Nenhum usuário encontrado'))
+  }
 
 
 	return <FacebookLogin
-    appId="675476209915681"
+    appId={process.env.REACT_APP_FACEBOOK_ID}
     autoLoad={false}
     fields="name,email,picture"
     callback={responseFacebook}
