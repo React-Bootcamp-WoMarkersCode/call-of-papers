fefd0b1246e7eb62c7c7a0855e4db567cd1fa7a2
Mariana Tancredi Alvares Fernandes <matancredi@hotmail.com>
2020-04-22 12:27:43 -0300

    Adicionando noopener noreferrer nos links pra resolver warning

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -14,17 +14,17 @@ const About = () => {
               Possibilitando que organizadores criem eventos e encontrem palestrantes que tenham interesse em participar desse evento.
               Os palestrantes podem cadastrar na plataforma as suas propostas de atividades e enviá-las aos organizadores dos eventos.
               Os organizadores também podem divulgar o interesse por patrocinadores e voluntários.</p>
-            <p>A plataforma <i>Sharing Talks</i> é o projeto final desenvolvido no <a href='https://womakerscode.org/' target="_blank"><strong>React Bootcamp da WoMakersCode</strong></a> como forma de aplicar os conhecimentos adquiridos ao longo do curso.
-              O projeto está disponível no <a href="https://github.com/React-Bootcamp-WoMarkersCode/call-of-papers" target="_blank">GitHub <GithubOutlined />.</a>
+            <p>A plataforma <i>Sharing Talks</i> é o projeto final desenvolvido no <a href='https://womakerscode.org/' target="_blank" rel="noopener noreferrer"><strong>React Bootcamp da WoMakersCode</strong></a> como forma de aplicar os conhecimentos adquiridos ao longo do curso.
+              O projeto está disponível no <a href="https://github.com/React-Bootcamp-WoMarkersCode/call-of-papers" target="_blank" rel="noopener noreferrer">GitHub <GithubOutlined />.</a>
             </p>
             <List>
               <Col span={12}>
               <h3>Desenvolvedoras</h3>
-              <List.Item>Alessandra Nastassja: <a href="https://github.com/Alessandra-Nastassja" target="_blank">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/alessandra-nastassja/" target="_blank">Linkedin <LinkedinOutlined /></a></List.Item>
-              <List.Item>Isabella Soares: <a href="https://github.com/IsabellaSoares" target="_blank">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/isabella-soares-lima/" target="_blank">Linkedin <LinkedinOutlined /></a></List.Item>
-              <List.Item>Kathleen Pallotta: <a href="https://github.com/kathleenpallotta" target="_blank">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/kathleen-pallotta-ab8346199/" target="_blank">Linkedin <LinkedinOutlined /></a></List.Item>
-              <List.Item>Mariana Tancredi: <a href="https://github.com/matancredi" target="_blank">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/mariana-t-52aa8b8b/" target="_blank">Linkedin <LinkedinOutlined /></a></List.Item>
-              <List.Item>Olívia Resende: <a href="https://github.com/oliviaresende" target="_blank">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/oliviasilvaresende/" target="_blank">Linkedin <LinkedinOutlined /></a></List.Item>
+              <List.Item>Alessandra Nastassja: <a href="https://github.com/Alessandra-Nastassja" target="_blank" rel="noopener noreferrer">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/alessandra-nastassja/" target="_blank" rel="noopener noreferrer">Linkedin <LinkedinOutlined /></a></List.Item>
+              <List.Item>Isabella Soares: <a href="https://github.com/IsabellaSoares" target="_blank" rel="noopener noreferrer">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/isabella-soares-lima/" target="_blank" rel="noopener noreferrer">Linkedin <LinkedinOutlined /></a></List.Item>
+              <List.Item>Kathleen Pallotta: <a href="https://github.com/kathleenpallotta" target="_blank" rel="noopener noreferrer">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/kathleen-pallotta-ab8346199/" target="_blank" rel="noopener noreferrer">Linkedin <LinkedinOutlined /></a></List.Item>
+              <List.Item>Mariana Tancredi: <a href="https://github.com/matancredi" target="_blank" rel="noopener noreferrer">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/mariana-t-52aa8b8b/" target="_blank" rel="noopener noreferrer">Linkedin <LinkedinOutlined /></a></List.Item>
+              <List.Item>Olívia Resende: <a href="https://github.com/oliviaresende" target="_blank" rel="noopener noreferrer">GitHub <GithubOutlined /></a> e <a href="https://www.linkedin.com/in/oliviasilvaresende/" target="_blank" rel="noopener noreferrer">Linkedin <LinkedinOutlined /></a></List.Item>
               </Col>
             </List>
           </Col>
