c7dbd9c7727d8f08d431bb6538fc6330efcc55a2
Isabella <isabella.soares.28@gmail.com>
2020-04-22 21:55:52 -0300

    Fix: vertical menu fixes

--- src/components/Menu/VerticalMenu.js
+++ src/components/Menu/VerticalMenu.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Menu, Avatar, Button, Drawer } from 'antd'
+import { Menu, Button, Drawer } from 'antd'
 import { MenuOutlined } from '@ant-design/icons'
 import './style.scss'
 
@@ -7,57 +7,88 @@ const VerticalMenu = ({ history, userPicture, userRole, logout }) => {
   const [ visible, setVisible ] = useState(false)
 
   return (
-    !localStorage.getItem('userId') ?
-      (<></>)
-        :
-      (<div className='menu-hamburguer'>
-        <Button className='btn-outline' onClick={() => setVisible(true)}>
-          <MenuOutlined />
-        </Button>
-        <Drawer
-          title={<>
-            <Avatar src={userPicture} style={{ marginRight: '1em' }} />
-            <span>{localStorage.getItem('userName')}</span>
-          </>}
-          placement="right"
-          closable={false}
-          onClose={() => setVisible(false)}
-          visible={visible}
-        >
-          <Menu theme='light' mode='vertical'>
-            <Menu.Item
-              key='/events'
-              disabled={userRole === 'Speaker'}
-              onClick={() => {
-                history.push('/events')
-                setVisible(false)
-              }}>
-              Sou produtor de eventos
-            </Menu.Item>
-            <Menu.Item
-              key='/lectures'
-              disabled={userRole === 'Producer'}
-              onClick={() => {
-                history.push('/lectures')
-                setVisible(false)
-              }}>
-              Sou palestrante
-            </Menu.Item>
-            <Menu.Item key='/profile' onClick={() => {
-              history.push('/profile')
+    <div className='menu-hamburguer'>
+      {
+        !localStorage.getItem('userId') ?
+          (<Button
+            type='default'
+            className='btn-outline'
+            onClick={() => {
+              history.push('/login')
+              setVisible(false)
+            }
+          }>
+            Login
+          </Button>) : (<></>)
+      }
+      <Button className='btn-outline' onClick={() => setVisible(true)}>
+        <MenuOutlined />
+      </Button>
+      <Drawer
+        title={<img src={require('../../assets/logo-mobile.png')} alt='Sharing Talks' className='logo-mobile' />}
+        placement="right"
+        closable={false}
+        onClose={() => setVisible(false)}
+        visible={visible}
+      >
+        {!localStorage.getItem('userId') ?
+          (<Menu theme='light' mode='vertical'>
+          <Menu.Item key='/events' onClick={() => {
+            window.location.href='#events'
+            setVisible(false)
+          }}>
+            Produtores de eventos
+          </Menu.Item>
+          <Menu.Item key='/lectures' onClick={() => {
+            window.location.href='#lectures'
+            setVisible(false)
+          }}>
+            Palestrantes
+          </Menu.Item>
+          <Menu.Item key='/highlights' onClick={() => {
+            window.location.href='#highlights'
+            setVisible(false)
+          }}>
+            Destaques
+          </Menu.Item>
+          </Menu>)
+            :
+          (<Menu theme='light' mode='vertical'>
+          <Menu.Item
+            key='/events'
+            disabled={userRole === 'Speaker'}
+            onClick={() => {
+              history.push('/events')
               setVisible(false)
             }}>
-              Meu perfil
-            </Menu.Item>
-            <Menu.Item key='logout' onClick={() => {
-              logout()
+            Sou produtor de eventos
+          </Menu.Item>
+          <Menu.Item
+            key='/lectures'
+            disabled={userRole === 'Producer'}
+            onClick={() => {
+              history.push('/lectures')
               setVisible(false)
             }}>
-              Sair
-            </Menu.Item>
-          </Menu>
-        </Drawer>
-      </div>)
+            Sou palestrante
+          </Menu.Item>
+          <Menu.Item key='/profile' onClick={() => {
+            history.push('/profile')
+            setVisible(false)
+          }}>
+            Meu perfil
+          </Menu.Item>
+          <Menu.Item key='logout' onClick={() => {
+            logout()
+            setVisible(false)
+          }}>
+            Sair
+          </Menu.Item>
+          </Menu>)
+        }
+
+      </Drawer>
+    </div>
   )
 }
 
