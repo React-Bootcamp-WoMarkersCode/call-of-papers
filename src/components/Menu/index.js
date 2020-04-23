1ac88ebec07b99a67703345fee7aafe7b2431e24
Isabella <isabella.soares.28@gmail.com>
2020-04-22 21:09:19 -0300

    Fix: set menu responsive

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useHistory, useLocation } from 'react-router-dom'
-import { Menu, Avatar, Button } from 'antd'
+import { Menu, Avatar, Button, Drawer } from 'antd'
+import { MenuOutlined } from '@ant-design/icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 import './style.scss'
@@ -13,6 +14,8 @@ const HeaderComponent = () => {
   const location = useLocation()
   const userPicture = localStorage.getItem('userPicture')
   const userRole = localStorage.getItem('userId') ? localStorage.getItem('userRole') : ''
+  const mobileViewport = (window.innerWidth <= 425 )
+  const [ visible, setVisible ] = useState(false)
 
   const logout = () => {
     localStorage.clear()
@@ -20,7 +23,7 @@ const HeaderComponent = () => {
   }
 
   return (
-    <div className="menu-container">
+    <div className={['menu-container', mobileViewport ? 'menu-mobile' : '']}>
       <Link to='/'>
         <img src={require('../../assets/logo.png')} alt='Sharing Talks' className='logo' />
         <img src={require('../../assets/logo-mobile.png')} alt='Sharing Talks' className='logo-mobile' />
@@ -31,21 +34,23 @@ const HeaderComponent = () => {
           <></>
             :
           (!localStorage.getItem('userId') ?
-            (<>
-              <Menu theme='light' mode='horizontal'>
-                <Menu.Item key='/events' onClick={() => window.location.href='#events'}>
-                  Produtores de eventos
-                </Menu.Item>
-                <Menu.Item key='/lectures' onClick={() => window.location.href='#lectures'}>
-                  Palestrantes
-                </Menu.Item>
-                <Menu.Item key='/highlights' onClick={() => window.location.href='#highlights'}>
-                  Destaques
-                </Menu.Item>
-              </Menu>
-              <Button type='default' className='btn-outline' onClick={() => history.push('/login')}>Login</Button>
-            </>)
+            ( !mobileViewport ?
+              (<>
+                <Menu theme='light' mode='horizontal'>
+                  <Menu.Item key='/events' onClick={() => window.location.href='#events'}>
+                    Produtores de eventos
+                  </Menu.Item>
+                  <Menu.Item key='/lectures' onClick={() => window.location.href='#lectures'}>
+                    Palestrantes
+                  </Menu.Item>
+                  <Menu.Item key='/highlights' onClick={() => window.location.href='#highlights'}>
+                    Destaques
+                  </Menu.Item>
+                </Menu>
+                <Button type='default' className='btn-outline' onClick={() => history.push('/login')}>Login</Button>
+              </>) : (<></>))
               :
+            ( !mobileViewport ?
             (<Menu theme='light' mode='horizontal'>
               <Menu.Item
                 key='/events'
@@ -69,7 +74,44 @@ const HeaderComponent = () => {
                   Sair
                 </Menu.Item>
               </SubMenu>
-            </Menu>))
+            </Menu>) : (
+            <div className='menu-hamburguer'>
+              <Button className='btn-outline' onClick={() => setVisible(true)}>
+                <MenuOutlined />
+              </Button>
+              <Drawer
+                title={<>
+                  <Avatar src={userPicture} style={{ marginRight: '1em' }} />
+                  <span>{localStorage.getItem('userName')}</span>
+                </>}
+                placement="right"
+                closable={false}
+                onClose={() => setVisible(false)}
+                visible={visible}
+              >
+                <Menu theme='light' mode='vertical'>
+                  <Menu.Item
+                    key='/events'
+                    disabled={userRole === 'Speaker'}
+                    onClick={() => history.push('/events')}>
+                    Sou produtor de eventos
+                  </Menu.Item>
+                  <Menu.Item
+                    key='/lectures'
+                    disabled={userRole === 'Producer'}
+                    onClick={() => history.push('/lectures')}>
+                    Sou palestrante
+                  </Menu.Item>
+                  <Menu.Item key='/profile' onClick={() => history.push('/profile')}>
+                    Meu perfil
+                  </Menu.Item>
+                  <Menu.Item key='logout' onClick={() => logout()}>
+                    Sair
+                  </Menu.Item>
+                </Menu>
+              </Drawer>
+            </div>))
+          )
       }
       </div>
     </div>
