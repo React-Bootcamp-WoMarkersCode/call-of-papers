896a7bc1fa8548f03eb433d60aeca1631964caa5
Olívia Resende <olivinharesende@hotmail.com>
2020-04-27 03:25:58 -0300

    botão google

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router'
 import { Link } from 'react-router-dom'
 import { Row, Card, Button, Col } from 'antd'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGoogle, faAt } from "@fortawesome/free-solid-svg-icons"
+import { faAt } from "@fortawesome/free-solid-svg-icons"
 import './style.scss'
 import FBLogin from './FBLogin'
 import GglLogin from './GglLogin'
@@ -66,14 +66,11 @@ const Login = () => {
       </Row>
       <Row className='btn-group' justify='center'>
         <FBLogin isDisabled={role === ''} event={eventId} role={role} />
+        <GglLogin disabled={role === ''} />
         <Button className='mail-button' disabled={role === ''} onClick={() => setMailLogin(true)}>
           <FontAwesomeIcon icon={faAt} />
           Continuar com o seu e-mail
         </Button>
-        {/* <Button type='primary' className='google-button' disabled>
-          <FontAwesomeIcon icon={faGoogle} />
-          Continuar com o Google
-        </Button> */}
         <p>Ao entrar, você concorda com os nossos <Link to="/termos-de-uso">Termos</Link> e <Link to="/politica-de-privacidade">Política de Privacidade</Link>.</p>
       </Row>
       <Row className='btn-group' justify='center'>
