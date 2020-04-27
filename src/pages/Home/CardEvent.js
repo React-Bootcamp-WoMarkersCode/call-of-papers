2a1cce1452ff720d44ffe9b19db9130761ca17e9
Isabella <isabella.soares.28@gmail.com>
2020-04-26 22:13:24 -0300

    Fix: add default event image

--- src/pages/Home/CardEvent.js
+++ src/pages/Home/CardEvent.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Card, Typography, Space } from 'antd'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faImage } from '@fortawesome/free-solid-svg-icons'
 
 const { Text } = Typography;
 
@@ -10,14 +12,17 @@ const CardEvent = ({ event }) => {
     <Card actions={[<Link to={`/events/${event.id}`} >Submeta sua palestra</Link>]} >
 
 
-      <Space direction="vertical">
-        <div style={{width: '100%',height: '150px',overflow: 'hidden'}}>
-          <img src={event.uploadedImage} alt="" width="100%" style={{ margin: '0' }} />
+      <Space direction='vertical' style={{width: '100%'}}>
+        <div className='event-img'>
+          { event.uploadedImage ?
+            (<img src={event.uploadedImage} alt='Imagem do evento' width='100%' height='100%' />) :
+            (<FontAwesomeIcon icon={faImage} />)
+          }
         </div>
         <Text strong>{event.event}</Text>
-        <Space direction="vertical" size={0}>
-          <Text type="secondary">{event.schedule}</Text>
-          <Text type="secondary">{event.local}</Text>
+        <Space direction='vertical' size={0}>
+          <Text type='secondary'>{event.schedule}</Text>
+          <Text type='secondary'>{event.local}</Text>
         </Space>
       </Space>
     </Card>
