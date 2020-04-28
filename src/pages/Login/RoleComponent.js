e48ce4c05f15454888f635604ff5bf90014c2b56
Isabella <isabella.soares.28@gmail.com>
2020-04-28 00:02:48 -0300

    Fix: correct indent

--- src/pages/Login/RoleComponent.js
+++ src/pages/Login/RoleComponent.js
@@ -1,16 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import { Row, Card, Button, Col, notification } from 'antd'
-import { useHistory } from 'react-router-dom'
-import { getEnvironment } from './../../utils/environment'
-import Header from './../../components/Header'
-import './style.scss'
+import React, { useState, useEffect } from 'react';
+import { Row, Card, Button, Col, notification } from 'antd';
+import { useHistory } from 'react-router-dom';
+import { getEnvironment } from './../../utils/environment';
+import Header from './../../components/Header';
+import './style.scss';
 
 const RoleComponent = () => {
-
-  const [role, setRole] = useState('')
-  const environment = getEnvironment()
-  const [ profile, setProfile ] = useState([])
-  const history = useHistory();
+	const [ role, setRole ] = useState('');
+	const environment = getEnvironment();
+	const [ profile, setProfile ] = useState([]);
+	const history = useHistory();
 
 	const openNotification = (type, description) => {
 		notification[type]({
@@ -19,99 +18,98 @@ const RoleComponent = () => {
 		});
 	};
 
-  useEffect(() => {
-		fetch(`${environment}/profiles/` + localStorage.getItem('userId'))
-			.then((res) => res.json())
-			.then((data) => {
-				setProfile(data.find((profile) => profile.id === localStorage.getItem('userId')))
+	useEffect(
+		() => {
+			fetch(`${environment}/profiles/` + localStorage.getItem('userId'))
+				.then((res) => res.json())
+				.then((data) => {
+					setProfile(data.find((profile) => profile.id === localStorage.getItem('userId')));
+				})
+				.catch((err) => console.error(err, 'Nenhum usuário encontrado'));
+		},
+		[ environment ]
+	);
+
+	const handleSubmit = () => {
+		fetch(`${environment}/profiles/` + localStorage.getItem('userId'), {
+			method: 'PATCH',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				...profile,
+				role: role
 			})
-			.catch((err) => console.error(err, 'Nenhum usuário encontrado'))
-  }, [environment])
+		})
+			.then(() => {
+				localStorage.setItem('userRole', role);
+				openNotification('success', 'Cadastro realizado com sucesso!');
+				history.push('/');
+			})
+			.catch((err) => {
+				console.error(err);
+				openNotification('error', 'Não foi possível finalizar seu cadastro! Tente novamente!');
+			});
+	};
 
-  const handleSubmit = () => {
-    fetch(`${environment}/profiles/` + localStorage.getItem('userId'), {
-      method: 'PATCH',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        ...profile,
-        role: role
-      })
-    })
-      .then(() => {
-        localStorage.setItem('userRole', role)
-        openNotification('success', 'Cadastro realizado com sucesso!');
-        history.push('/')
-      })
-      .catch((err) => {
-        console.error(err)
-        openNotification('error', 'Não foi possível finalizar seu cadastro! Tente novamente!')
-      })
-  }
+	return (
+		<>
+			<Header text="QUEM É VOCÊ ?" />
+			<Row style={{ marginBottom: '1rem', fontSize: '16px', fontWeight: 'bold' }}>Complete o seu cadastro!</Row>
+			<Row style={{ marginBottom: '1rem' }}>Depois é possível mudar essa opção no seu perfil.</Row>
+			<Row justify="space-between">
+				<Col xs={{ span: 12 }} md={{ span: 6 }}>
+					<Card
+						hoverable
+						className={[ 'login-card', role === 'Producer' ? 'card-selected' : '' ]}
+						onClick={() => setRole('Producer')}
+					>
+						<Row justify="center">
+							<img src={require('../../assets/events-producer.png')} alt="Produtor de evento" />
+							<span className="card-span">Sou produtor de eventos</span>
+						</Row>
+					</Card>
+				</Col>
+				<Col xs={{ span: 12 }} md={{ span: 6 }}>
+					<Card
+						hoverable
+						className={[ 'login-card', role === 'Speaker' ? 'card-selected' : '' ]}
+						onClick={() => setRole('Speaker')}
+					>
+						<Row justify="center">
+							<img src={require('../../assets/speaker.png')} alt="Palestrante" />
+							<span className="card-span">Sou palestrante</span>
+						</Row>
+					</Card>
+				</Col>
+				<Col xs={{ span: 12 }} md={{ span: 6 }}>
+					<Card
+						hoverable
+						className={[ 'login-card', role === 'Both' ? 'card-selected' : '' ]}
+						onClick={() => setRole('Both')}
+					>
+						<Row justify="center">
+							<img src={require('../../assets/both.png')} alt="Produtor e palestrante" />
+							<span className="card-span">Sou ambos</span>
+						</Row>
+					</Card>
+				</Col>
+			</Row>
+			<Row justify="center" style={{ marginTop: '1.5rem' }}>
+				<Button
+					type="default"
+					className="btn-outline"
+					disabled={role === ''}
+					onClick={() => {
+						handleSubmit();
+					}}
+				>
+					Completar cadastro
+				</Button>
+			</Row>
+		</>
+	);
+};
 
-  return(
-    <>
-      <Header text="QUEM É VOCÊ ?" />
-      <Row style={{ marginBottom: '1rem', fontSize: '16px', fontWeight: 'bold'}}>
-        Complete o seu cadastro!
-      </Row>
-      <Row style={{ marginBottom: '1rem'}}>
-        Depois é possível mudar essa opção no seu perfil.
-      </Row>
-      <Row justify='space-between'>
-        <Col xs={{ span: 12 }} md={{ span: 6 }}>
-          <Card
-            hoverable
-            className={['login-card', (role === 'Producer' ? 'card-selected' : '')]}
-            onClick={() => setRole('Producer')}>
-            <Row justify='center'>
-              <img
-                src={require('../../assets/events-producer.png')}
-                alt='Produtor de evento' />
-              <span className='card-span'>Sou produtor de eventos</span>
-            </Row>
-          </Card>
-        </Col>
-        <Col xs={{ span: 12 }} md={{ span: 6 }}>
-          <Card
-            hoverable
-            className={['login-card', (role === 'Speaker' ? 'card-selected' : '')]}
-            onClick={() => setRole('Speaker')}>
-            <Row justify='center'>
-              <img
-                src={require('../../assets/speaker.png')}
-                alt='Palestrante' />
-              <span className='card-span'>Sou palestrante</span>
-            </Row>
-          </Card>
-        </Col>
-        <Col xs={{ span: 12 }} md={{ span: 6 }}>
-          <Card
-            hoverable
-            className={['login-card', (role === 'Both' ? 'card-selected' : '')]}
-            onClick={() => setRole('Both')}>
-            <Row justify='center'>
-              <img
-                src={require('../../assets/both.png')}
-                alt='Produtor e palestrante' />
-              <span className='card-span'>Sou ambos</span>
-            </Row>
-          </Card>
-        </Col>
-      </Row>
-      <Row justify='center' style={{ marginTop: '1.5rem'}}>
-        <Button type='default' className='btn-outline' disabled={role === ''}
-          onClick={() => {
-            handleSubmit()
-          }}
-        >
-          Completar cadastro
-        </Button>
-      </Row>
-    </>
-  )
-}
-
-export default RoleComponent
+export default RoleComponent;
