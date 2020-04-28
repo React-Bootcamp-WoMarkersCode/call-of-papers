6c0a4635c1a458c33d8f16dbf8e4a6e03e2852da
Isabella <isabella.soares.28@gmail.com>
2020-04-28 19:29:31 -0300

    Fix: add Heroku server

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -1,3 +1,4 @@
 export function getEnvironment(){
-    return `http://${process.env.REACT_APP_ENVIROMENT}:3001`
+    // return `http://${process.env.REACT_APP_ENVIROMENT}:3001`
+    return process.env.REACT_APP_ENVIROMENT
 }
