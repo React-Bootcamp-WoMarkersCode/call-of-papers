f8290eda340710e7a84eb2e642b3e3523035b368
GabrieleSuzart <gabrielesuzart@gmail.com>
2020-04-28 15:02:16 -0300

    test

--- src/utils/Email/Email.js
+++ src/utils/Email/Email.js
@@ -4,7 +4,8 @@ function Email (name, email, message) {
         email,
         message
     }
-    fetch('http://localhost:3333/send', {
+    // fetch('http://localhost:3333/send', {
+    fetch('http://172.31.92.96:3333/send', {
         method: 'POST',
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(data)
